bf8f64c0b120786e558b1d4558059e3c3b678847
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-03-07 20:03:29 -0300

    Fourth Requirement - partial-Lint

--- src/pages/helpers/Form.js
+++ src/pages/helpers/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Form extends React.Component {
